b89bdb730b07cf14848077458b21423ff7607910
Sulaiman Khan <sk2537987@gmail.com>
2026-04-21 01:25:57 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const mongoose = require('mongoose');
+
+main().then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://localhost:27017/mydatabase1');
+}
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
