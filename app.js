106312013f9293c822fa716c9a8a17bd209aec81
Sulaiman Khan <sk2537987@gmail.com>
2026-04-24 20:06:48 +0500

    added custom css styles and also make it beautiful.

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(methodOverride('_method'));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));    
+app.use(express.static(path.join(__dirname, "public")));
 
 
 main().then(() => console.log('Connected to MongoDB'))
